ee5b7ff40c4df10d009a436464d7714b5ef086ca
Sava <vuckovicsava11@gmail.com>
2018-02-28 09:36:15 +0100

    eliminating colors

--- src/ai_knuth_brain.rb
+++ src/ai_knuth_brain.rb
@@ -5,28 +5,62 @@ class AIBrain
 
   @@possible_codes = nil
   @@guess = nil
-  @@possible_colors = Array.new(Board::POSSIBLE_COLORS)
 
   def self.reset
     @@possible_codes = (1111..8888).to_a
     @@guess = nil
   end
 
-  # only method that can update state of lookup
+  # receive feedback about our latest move
   def self.set_feedback state
+    @@feedback = state
 
-
+    eliminate_codes
   end
 
   def self.generate_guess
-
+    if @@guess
+      @@possible_codes.shuffle.first
+    else
+      @@guess = guess_translator(1122)
+    end
+    puts 'GUESSSS: ' + @@guess
+    @@guess
   end
 
   private
+  # convert guess from numbers like 1111 to strings like 'RRRR'
   def self.guess_translator guess
     (guess.to_s.split('').collect do |num_str|
       index = num_str.to_i - 1
-      @@possible_colors[index]
+      Board::POSSIBLE_COLORS[index]
     end).join('')
+  end
+
+  def self.eliminate_codes
+    # filter the array of possible codes and remove all that don't give the same result
+    @@possible_codes.select! do |c|
+       process_number(c) == @@feedback # 1123
+    end
+  end
+
+  def self.process_number n
+    code = @@guess
+    guess = guess_translator n
+
+    # colors
+    puts "CODE: #{code}"
+    puts "GUESS: #{guess}"
+    similar_colors = (code.split('') & guess.split('')).length
+    # positions
+    non_matches = 0
+    code.split('').each_with_index do |val, idx|
+      non_matches += 1 if val != guess[idx]
+    end
+
+    return {
+      colors: similar_colors,
+      positions: 4 - non_matches
+    }
   end
 end
