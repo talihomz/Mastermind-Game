195b99b3de6545c89dfc00948fd2642de655760d
Sava Vuckovic <vuckovicsava11@gmail.com>
2018-03-12 19:42:36 +0100

    decided basic methods game should have

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,4 +2,20 @@ require "game"
 
 describe Game do 
 
+    describe "#initialize" do 
+    
+    end
+
+    describe "#guess_code" do 
+    
+    end
+
+    describe "#reset" do 
+
+    end
+
+    describe "#set_code" do
+
+    end
+
 end
